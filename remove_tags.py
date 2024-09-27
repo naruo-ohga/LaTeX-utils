f8c3ccce6943b0e06e9a923ff88fb439406c14cd
naruo-ohga <129951497+naruo-ohga@users.noreply.github.com>
2024-09-28 00:53:09 +0900

    Debug

--- remove_tags.py
+++ remove_tags.py
@@ -3,11 +3,11 @@ import os
 import sys
 
 subtext = {'math':  "'X'",
-           '~eqref': " (57)",
+           '~eqref': "~(57)",
            'eqref': "(58)",
-           '~ref':   " (59)",
+           '~ref':   "~(59)",
            'ref':   "(60)",
-           '~cite':  " [61]",
+           '~cite':  "~[61]",
            'cite':  "[62]",
            'env,':   '"ENVIRONMENT,"',
            'env.':   '"ENVIRONMENT."',
@@ -37,14 +37,14 @@ if len(sys.argv) != 3:
     sys.exit(1)
 mode = sys.argv[1]
 tex_file = sys.argv[2]
-assert(tex_file.endswith('.tex'))
+if not tex_file.endswith('.tex'):
+    print("Error: tex_file must end with .tex")
+    sys.exit(1)
 record_file = tex_file[:-4] + "_record.txt"
-backup_file = tex_file[:-4] + "_backup.tex"
+stripped_file = tex_file[:-4] + "_stripped.tex"
+restored_file = tex_file[:-4] + "_restored.tex"
 
 if mode == "remove":
-    # Make backup of the tex file
-    os.system(f"cp {tex_file} {backup_file}")
-
     # Read the tex file
     with open(tex_file, "r") as f:
         tex = f.read()
@@ -56,14 +56,14 @@ if mode == "remove":
         tex = re.sub(regex[key], subtext[key], tex)
 
     # Output the result
-    with open(tex_file, "w") as f:
+    with open(stripped_file, "w") as f:
         f.write(tex)
 
     # Output the removed tags
     with open(record_file, "w") as f:
         for key, tags in record.items():
             for tag in tags:
-                f.write(f'###{key}### ####{tag}####\n')
+                f.write(f'###{key}#### #####{tag}######\n')
         
 elif mode == "restore":
     # Read the removed tags
@@ -73,14 +73,14 @@ elif mode == "restore":
     # Parse the removed tags
     record = {}
     for key in subtext.keys():
-        record[key] = [x.group() for x in re.finditer(f'###{key}### ####(.*?)####', record_text)]
+        record[key] = [x.group(1) for x in re.finditer(f'###{re.escape(key)}#### #####([\s\S]*?)######', record_text)]
 
     # Read the tex file
-    with open(tex_file, "r") as f:
+    with open(stripped_file, "r") as f:
         tex = f.read()
 
     # Restore the tags
-    for key, rec in record.items():
+    for key, rec in record.items().__reversed__():
         nfield = tex.count(subtext[key])
         if nfield != len(rec): 
             print(f'Warning: # of {key}s does not match: {nfield} fields found but {len(rec)} records given')
@@ -88,7 +88,7 @@ elif mode == "restore":
             tex = tex.replace(subtext[key], r, 1)
 
     # Output the result
-    with open(tex_file, "w") as f:
+    with open(restored_file, "w") as f:
         f.write(tex)
 
 else:
