6be7d0a8444c153f51885816ef7cbbbc32b350a6
naruo-ohga <129951497+naruo-ohga@users.noreply.github.com>
2024-09-28 12:38:27 +0900

    Add split_sections

--- remove_tags.py
+++ remove_tags.py
@@ -1,28 +1,66 @@
 import re
 import os
 import sys
+import itertools
 
-subtext = {'math':  "'X'",
+
+splitter = [r'\\begin\{document\}', r'\\section\{.*?\}', r'\\subsection\{.*?\}', r'\\subsubsection\{.*?\}', r'\\paragraph\{.*?\}', r'\\subparagraph\{.*?\}']
+
+def split_sections(tex):
+    splitter_regex = "(" + "|".join(splitter) + ")"
+    sections_regex = re.split(splitter_regex, tex)
+
+    titles = []
+    sections = []
+    current_title = ""
+    current_section = ""
+    
+    for sec in sections_regex:
+        if re.fullmatch(splitter_regex, sec) is not None:
+            # Save the section
+            if current_section:
+                titles.append(current_title)
+                sections.append(current_section)
+
+            # Start the new section
+            current_title = sec
+            current_section = sec
+        else:
+            current_section += sec
+
+    if current_section:
+        titles.append(current_title)
+        sections.append(current_section)
+
+    # Check consistency
+    assert(len(titles) == len(sections))
+    assert(tex == "".join(sections))
+
+    return titles, sections
+
+
+
+subtext = {'math':   "'X'",
            '~eqref': "~(57)",
-           'eqref': "(58)",
+           'eqref':  "(58)",
            '~ref':   "~(59)",
-           'ref':   "(60)",
+           'ref':    "(60)",
            '~cite':  "~[61]",
-           'cite':  "[62]",
+           'cite':   "[62]",
            'env,':   '"ENVIRONMENT,"',
            'env.':   '"ENVIRONMENT."',
-           'env':   '"ENVIRONMENT"',
+           'env':    '"ENVIRONMENT"',
           }
 
 regex = {'math':   r'\$.*?\$',
-         '~eqref':  r'~\\eqref\{.*?\}',
+         '~eqref': r'~\\eqref\{.*?\}',
          'eqref':  r'\\eqref\{.*?\}',
-         '~ref':    r'~\\ref\{.*?\}',
+         '~ref':   r'~\\ref\{.*?\}',
          'ref':    r'\\ref\{.*?\}',
-         '~cite':   r'~\\cite\{.*?\}',
+         '~cite':  r'~\\cite\{.*?\}',
          'cite':   r'\\cite\{.*?\}',
-         'env,':    r'\\begin\{(.*?)\}(?:(?!\\begin|\\end)[\s\S])*,(?:(?!\\begin|\\end)[^,\.])*\\end\{\1\}',
-         'env.':    r'\\begin\{(.*?)\}(?:(?!\\begin|\\end)[\s\S])*\.(?:(?!\\begin|\\end)[^,\.])*\\end\{\1\}',
+         'env,':   r'\\begin\{(.*?)\}(?:(?!\\begin|\\end)[\s\S])*,(?:(?!\\begin|\\end)[^,\.])*\\end\{\1\}',
+         'env.':   r'\\begin\{(.*?)\}(?:(?!\\begin|\\end)[\s\S])*\.(?:(?!\\begin|\\end)[^,\.])*\\end\{\1\}',
          'env':    r'\\begin\{(.*?)\}(?:(?!\\begin|\\end)[\s\S])*\\end\{\1\}',
         }
 
@@ -45,51 +83,84 @@ stripped_file = tex_file[:-4] + "_stripped.tex"
 restored_file = tex_file[:-4] + "_restored.tex"
 
 if mode == "remove":
-    # Read the tex file
+    # Read the tex file and split it into sections
     with open(tex_file, "r") as f:
         tex = f.read()
+    titles, sections = split_sections(tex)
 
-    # Replace the tags with the subtext
-    record = {}
-    for key in subtext.keys():
-        record[key] = [x.group() for x in re.finditer(regex[key], tex)]
-        tex = re.sub(regex[key], subtext[key], tex)
+    # Open output files
+    f_stripped = open(stripped_file, "w")
+    f_record = open(record_file, "w")
 
-    # Output the result
-    with open(stripped_file, "w") as f:
-        f.write(tex)
+    for title, sec in zip(titles, sections):
+        # Replace the tags with the subtext
+        record = {}
+        for key in subtext.keys():
+            record[key] = [x.group() for x in re.finditer(regex[key], sec)]
+            sec = re.sub(regex[key], subtext[key], sec)
 
-    # Output the removed tags
-    with open(record_file, "w") as f:
+        # Output the result
+        f_stripped.write(sec)
+
+        # Output the removed tags
+        f_record.write("\n" + title + "\n")
         for key, tags in record.items():
             for tag in tags:
-                f.write(f'###{key}#### #####{tag}######\n')
-        
+                f_record.write(f'###{key}#### #####{tag}######\n')
+    
+    f_stripped.close()
+    f_record.close()
+
+
 elif mode == "restore":
-    # Read the removed tags
+    # Read the removed tags and split it into sections
     with open(record_file, "r") as f:
         record_text = f.read()
+    titles1, record_sections = split_sections(record_text)
     
-    # Parse the removed tags
-    record = {}
-    for key in subtext.keys():
-        record[key] = [x.group(1) for x in re.finditer(f'###{re.escape(key)}#### #####([\s\S]*?)######', record_text)]
-
-    # Read the tex file
+    # Read the stripped file and split it into sections
     with open(stripped_file, "r") as f:
         tex = f.read()
+    titles2, tex_sections = split_sections(tex)
 
-    # Restore the tags
-    for key, rec in record.items().__reversed__():
-        nfield = tex.count(subtext[key])
-        if nfield != len(rec): 
-            print(f'Warning: # of {key}s does not match: {nfield} fields found but {len(rec)} records given')
-        for r in rec[:nfield]:
-            tex = tex.replace(subtext[key], r, 1)
+    # Check consistency
+    if len(titles1) != len(titles2):
+        print("Error: # of sections do not match")
+        print(f"# of sections in record: {len(titles1)}")
+        print(f"# of sections in stripped: {len(titles2)}")
+        print("Record || Stripped")
+        for t1, t2 in itertools.zip_longest(titles1, titles2):
+            print(f"{t1} || {t2}")
+        sys.exit(1)
 
-    # Output the result
-    with open(restored_file, "w") as f:
-        f.write(tex)
+    # Open the output file
+    f_restored = open(restored_file, "w")
+
+    for title1, title2, record_sec, tex_sec in zip(titles1, titles2, record_sections, tex_sections):
+        print(f"===== {title1} =====")
+        if not title1 == title2:
+            print("Warning: Titles do not match")
+            print("Title_recorded:", title1)
+            print("Title_stripped:", title2)
+            
+        # Parse the removed tags
+        record = {}
+        for key in subtext.keys():
+            record[key] = [x.group(1) for x in re.finditer(f'###{re.escape(key)}#### #####([\s\S]*?)######', record_sec)]
+
+        # Restore the tags
+        for key, rec in record.items().__reversed__():
+            nfield = tex_sec.count(subtext[key])
+            if nfield != len(rec): 
+                print(f'Warning: # of {key}s does not match: {nfield} fields found but {len(rec)} records given')
+            for r in rec[:nfield]:
+                tex_sec = tex_sec.replace(subtext[key], r, 1)
+
+        # Output the result
+        f_restored.write(tex_sec)
+
+    f_restored.close()
+
 
 else:
     print("Usage: remove_tags.py <mode> <tex_file>")
